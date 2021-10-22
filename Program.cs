488545f4c6c76b3880c32c160ee8839b63a8d933
madebymaryyy <92998063+madebymaryyy@users.noreply.github.com>
2021-10-22 20:53:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Firsttask_Marat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Napište kolik je teď hodin (například: 12): ");
+            Console.WriteLine("POZOR: Nepište desetinné číslo, to ukončí program!\n");
+            int time;
+            time = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nNapiste svoje jmeno: ");
+            string name;
+            name = Console.ReadLine( );
+
+            if (time < 12)
+            {
+                Console.Clear();
+                Console.WriteLine("\nDobré ráno, ti přeji " +  name);
+                Console.WriteLine("\nNyní je: " + time + "hodin.");
+            }
+
+            else if (time >= 12 && time < 18)
+            {
+                Console.Clear();
+                Console.WriteLine("\nDobré odpoledne, ti přeji " + name);
+                Console.WriteLine("\nNyní je: " + time + "hodin.");
+            }
+
+            else if (time >= 18 && time < 25)
+            {
+                Console.Clear();
+                Console.WriteLine("\nDobrý večer, ti přeji " + name);
+                Console.WriteLine("\nNyní je: " + time + "hodin.");
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Chyba!" + name + " , zkontrolujte prosím, zda jste zadali číslo od 0 do 24!");
+                Console.WriteLine("Také prosím zkontrolujte zda jste zadali !JENOM! číslo a nenapsali jste omylem slovo hodin nebo tečku.");
+            }
+
+
+
+        }
+    }
+}
